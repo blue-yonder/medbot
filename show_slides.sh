3ea7c5b3b67d390bd01609be292747d6873bf88f
Florian Wilhelm <Florian.Wilhelm@gmail.com>
2015-06-11 11:40:07 +0200

    Outline slide added

--- show_slides.sh
+++ show_slides.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
-ipython nbconvert medbot.ipynb --to slides --post serve 
+pip install -r slides_requirements.txt
+ipython nbconvert medbot.ipynb --to slides --post serve --reveal-prefix http://cdn.jsdelivr.net/reveal.js/2.6.2
